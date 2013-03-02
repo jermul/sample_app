1e2e2fa82c02b86097e758098c26330926651b4e
Jeremy Muljana <jermul714@gmail.com>
2013-03-01 18:48:19 -0800

    fix cart path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,11 @@ class ApplicationController < ActionController::Base
   private
 
     def current_cart
-      Cart.find(session[:cart_id])
+      @current_user ||= Cart.find(session[:cart_id])
     rescue ActiveRecord::RecordNotFound
       cart = Cart.create
       session[:cart_id] = cart.id 
       cart
     end
+    helper_method :current_cart
 end
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,17 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 SampleApp::Application.initialize!
+
+SampleApp::Application.configure do
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+  	address:          "smtp.gmail.com",
+  	port:             587,
+  	domain:           "domain.of.sender.net",
+  	authentication:   "plain",
+  	user_name:        "jermul714@gmail.com",
+  	password:         "6&ninenedlog",
+  	enable_starttls_auto: true
+  }
+end
